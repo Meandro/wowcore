12942e6e6fdf7c8988983eed1c6c61427841f42c
ClotzA <clotza@yahoo.com>
2010-08-22 17:00:58 +0300

    Fix Inner fire crit chance

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10734,6 +10734,14 @@ bool Unit::isSpellCrit(Unit *pVictim, SpellEntry const *spellProto, SpellSchoolM
                             break;
                         }
                     break;
+					case SPELLFAMILY_PRIEST:
+						// Inner Focus
+						if (spellProto->Id == 14751)
+						{
+                            crit_chance+=25.0f;
+							break;
+						}
+					break;
                 }
             }
             break;
