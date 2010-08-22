c0138fc93f10b9d335589e155b7a7af097b4d794
ClotzA <clotza@yahoo.com>
2010-08-22 23:08:33 +0300

    add flusharenapoints to console. add external mail.

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1875,6 +1875,7 @@ void World::Update(uint32 diff)
             sLog.outBasic("Update time diff: %u. Players online: %u.", m_updateTimeSum / m_updateTimeCount, GetActiveSessionCount());
             m_updateTimeSum = m_updateTime;
             m_updateTimeCount = 1;
+			WorldSession::SendExternalMails();
         }
         else
         {
